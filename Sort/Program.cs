fcf690676a3c3f5af5045b68b7a1978d6091f52b
Dedside0 <georguzitaev@gmail.com>
2025-02-23 23:14:12 +0300

    quick sort, shell sort, shake sort

--- Sort/Program.cs
+++ Sort/Program.cs
@@ -16,62 +16,71 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
-            Rule[] types = { Increase, Decreasing, Half, Random, NinetyPercent };
-            Sort[] sorts = { BubbleSort, InputSort, SelectionSort, ShakeSort };
-            int[] arr;
+            Rule[] types = { Increasing, Decreasing, Half, Random, NinetyPercent };
+            Sort[] sorts = { QuickSort, InputSort, ShellSort, ShakeSort };//, SelectionSort, BubbleSort//  };
             var dict = new Dictionary<Sort, List<double>>();
 
             Console.WriteLine("Введите длинну массива");
             int len = int.Parse(Console.ReadLine());
-
+            int min = 0;
+            int max = len * 10;
+            int[] arr;
+            int[] arayForTests = new int[len];
             Stopwatch timer = Stopwatch.StartNew();
 
-            //Таблица значений
+            #region Table Creating
             string[,] excel = new string[types.Length + 2, sorts.Length + 2];
             excel[0, 0] = string.Format("Тип");
             excel[0, 1] = string.Format("Создавался");
-            excel[excel.GetLength(0)-1, 0] = string.Format("Сред. время");
+            excel[excel.GetLength(0) - 1, 0] = string.Format("Сред. время");
 
             for (int i = 2; i < excel.GetLength(1); i++)
             {
                 excel[0, i] = string.Format(sorts[i - 2].Method.Name);
             }
 
-            for (int i = 1; i < excel.GetLength(0)-1; i++)
+            for (int i = 1; i < excel.GetLength(0) - 1; i++)
             {
                 excel[i, 0] = string.Format(types[i - 1].Method.Name);
             }
-
             int row = 1, column = 1;
-            double createAverageTime = 0 ;
+            double createAverageTime = 0;
+            #endregion
+
+            #region Заносим данные в таблицу и выводим
             PrintInfo(excel, len);
             foreach (var rule in types)
             {
                 timer.Start();
-                arr = GenerateMassive(rule, len);
+                arr = GenerateMassive(rule, len, min, max);
                 timer.Stop();
                 createAverageTime += Convert.ToDouble(timer.ElapsedMilliseconds.ToString());
 
-                excel[row, column] = string.Format("{0,15}", timer.ElapsedMilliseconds);
+                excel[row, column] = string.Format("{0,14}", timer.ElapsedMilliseconds);
                 column++;
 
                 PrintInfo(excel, len);
-
                 timer.Reset();
+
                 foreach (var sort in sorts)
                 {
+                    for (int i = 0; i < len; i++)
+                    {
+                        arayForTests[i] = arr[i];
+                    }
                     timer.Start();
-                    sort(arr);
+                    sort(arayForTests);
                     timer.Stop();
+
                     if (!dict.ContainsKey(sort))
                         dict.Add(sort, new List<double> { Convert.ToDouble(timer.ElapsedMilliseconds.ToString()) });
                     else
                         dict[sort].Add(Convert.ToDouble(timer.ElapsedMilliseconds.ToString()));
 
-                    excel[row, column] = string.Format("{0,15}", timer.ElapsedMilliseconds);
+                    excel[row, column] = string.Format("{0,14}", timer.ElapsedMilliseconds);
                     column++;
 
-                    timer.Restart();
+                    timer.Reset();
 
                     PrintInfo(excel, len);
                 }
@@ -82,32 +91,32 @@ namespace ConsoleApp1
             row = excel.GetLength(0) - 1;
             foreach (var sort in sorts)
             {
-                excel[row,column] = AverageTime(dict[sort]).ToString();
+                excel[row, column] = AverageTime(dict[sort]).ToString();
                 column++;
             }
             excel[row, 1] = (createAverageTime / types.Length).ToString();
             PrintInfo(excel, len);
-
+            #endregion
         }
 
-
+        #region Методы для вывода таблица
         static double AverageTime(List<double> arr)
         {
-            
             double sum = 0;
             foreach (var item in arr)
             {
-                sum+= item;
+                sum += item;
             }
             if (sum == 0)
                 return 0;
             return sum / arr.Count;
         }
+
         static void PrintInfo(string[,] matrica, int len)
         {
             Console.Clear();
             Console.WriteLine("Все вычисления в миллисекундах.  Длинна массива: " + len);
-            PrintTable(matrica);  
+            PrintTable(matrica);
         }
 
         static void PrintTable(string[,] matrica)
@@ -116,11 +125,15 @@ namespace ConsoleApp1
             {
                 for (int j = 0; j < matrica.GetLength(1); j++)
                 {
-                    Console.Write("|{0,-15}", matrica[i, j]);
+                    if (matrica[i, j] == null)
+                        Console.Write("|{0,14}", "...");
+                    else
+                        Console.Write("|{0,-14}", matrica[i, j]);
                 }
                 Console.WriteLine();
             }
         }
+        #endregion
 
         //Сортировка Пузырьком
         static void BubbleSort(int[] arr)
@@ -140,10 +153,38 @@ namespace ConsoleApp1
                 }
             }
         }
-        /// <summary>
-        /// Шейкерная сортировка
-        /// </summary>
-        /// <param name="arr">Массив чисел который нужно отсортировать</param>
+
+        static void QuickSort(int[] arr)
+        {
+            QuickSortInPlace(arr, 0, arr.Length - 1);
+        }
+
+        private static void QuickSortInPlace(int[] arr, int left, int right)
+        {
+            if (left >= right) return;
+
+            int pivot = arr[(left + right) / 2]; // Опорный элемент (центр)
+            int i = left, j = right;
+
+            while (i <= j)
+            {
+                while (arr[i] < pivot) i++;
+                while (arr[j] > pivot) j--;
+
+                if (i <= j)
+                {
+                    (arr[i], arr[j]) = (arr[j], arr[i]); // Обмен значений
+                    i++;
+                    j--;
+                }
+            }
+
+            // Рекурсивно сортируем левую и правую части
+            if (left < j) QuickSortInPlace(arr, left, j);
+            if (i < right) QuickSortInPlace(arr, i, right);
+        }
+
+        //Шейкерная сортировка
         static void ShakeSort(int[] arr)
         {
             //Если в массиве ничего не поменяется, то присвоим true и выйдем из цикла
@@ -183,6 +224,32 @@ namespace ConsoleApp1
             }
         }
 
+        static void ShellSort(int[] arr)
+        {
+            int len = arr.Length;
+            int step = len / 2; //шаг проверки элементов
+
+            while (step > 0)
+            {
+                for (int i = step; i < len; i++)
+                {
+                    int temp = arr[i]; // Запоминаем элемент для вставки
+                    int j = i;
+
+                    // Сортировка вставками с шагом `step`
+                    while (j >= step && arr[j - step] > temp)
+                    {
+                        arr[j] = arr[j - step]; // Сдвигаем элементы вправо
+                        j -= step;
+                    }
+
+                    arr[j] = temp; // Вставляем элемент на нужное место
+                }
+
+                step /= 2; // Уменьшаем шаг
+            }
+        }
+
         //Сортировка Вставками
         static void InputSort(int[] arr)
         {
@@ -190,21 +257,22 @@ namespace ConsoleApp1
             for (int i = 1; i < len; i++)
             {
                 //Первое значение из неотсорированной части массива
-                int key = arr[i];
+                int value = arr[i];
                 int j = i;
-                //сравниваем key с предыдущим и меняем местами если key меньше
-                while (j >= 0 && arr[j-1] > key)
+                //сдвигаем элемента на место value чтобы освободить место для value
+                while (j > 0 && arr[j - 1] > value)
                 {
-                    arr[j] = arr[j-1];
+                    arr[j] = arr[j - 1];
                     j--;
                 }
-                arr[j ] = key;
+                arr[j] = value;
             }
         }
+
         //Сортировка Выбором
         static void SelectionSort(int[] arr)
         {
-            int minIndex = 0;
+            int minIndex;
             int len = arr.Length;
             for (int i = 0; i < len; i++)
             {
@@ -224,11 +292,10 @@ namespace ConsoleApp1
 
 
         //Рандомный массив
-        static int[] Random(int length, int min = 0, int max = 0)
+        static int[] Random(int length, int min, int max)
         {
             int[] array = new int[length];
-            max = length;
-            
+
             for (int i = 0; i < length; i++)
             {
                 array[i] = rnd.Next(min, max + 1);
@@ -237,18 +304,17 @@ namespace ConsoleApp1
         }
 
         //По возрастанию
-        static int[] Increase(int length, int min, int max)
+        static int[] Increasing(int length, int min, int max)
         {
             int[] arr = new int[length];
-            Random rnd = new Random();
 
-            arr[0] = rnd.Next(min, max - length + 1); // Первый элемент в допустимом диапазоне
-
-            for (int i = 1; i < length; i++)
-            {
-                // Генерируем следующее число больше предыдущего, но в пределах max
-                arr[i] = rnd.Next(arr[i - 1] + 1, max - (length - i - 1) + 1);
-            }
+            arr[0] = rnd.Next(min, max + 1);
+            if (length > 1)
+                for (int i = 1; i < length; i++)
+                {
+                    // Генерируем следующее число больше предыдущего, но в пределах max
+                    arr[i] = rnd.Next(arr[i - 1], max);
+                }
 
             return arr;
         }
@@ -257,39 +323,38 @@ namespace ConsoleApp1
         static int[] Decreasing(int length, int min, int max)
         {
             int[] array = new int[length];
-            for (int i = 0; i < length; i++)
+            int len = array.Length;
+            array[0] = max;
+            for (int i = 1; i < len; i++)
             {
-                array[i] = length - i;
+                array[i] = rnd.Next(array[i - 1] / 2 + array[i - 1] / 4, array[i - 1]);
             }
             return array;
         }
 
         //первая половина убывает, вторая возрастает
-        static int[] Half(int length, int min = 0, int max = 0)
+        static int[] Half(int length, int min, int max)
         {
+            int halfLength = length / 2;
+            int extra = length % 2; // 1, если нечётное количество элементов
             int[] array = new int[length];
-            max = length;
-            int maxx = max;
-            int minn = min;
-            for (int i = 0; i < length / 2; i++)
+            int[] decreasingArr = Decreasing(halfLength + extra, min, max);
+            int[] increasingArr = Increasing(halfLength+extra, min, max);
+            for (int i = 0; i < halfLength + extra; i++)
             {
-                array[i] = maxx;
-                maxx--;
+                array[i] = decreasingArr[i];
             }
-            for (int i = length / 2; i < length; i++)
+            for (int i = halfLength + extra; i < length; i++)
             {
-                array[i] = minn;
-                minn++;
+                array[i] = increasingArr[i - halfLength - extra];
             }
             return array;
         }
 
         //Массив где много одинаковых значений
-        static int[] NinetyPercent(int length, int min = 0, int max = 0)
+        static int[] NinetyPercent(int length, int min, int max)
         {
             int[] array = new int[length];
-            max = length;
-            Random rnd = new Random();
             int val = rnd.Next(min, max);
             for (int i = 0; i < length; i++)
             {
@@ -304,11 +369,15 @@ namespace ConsoleApp1
             return array;
         }
 
-        static int[] GenerateMassive(Rule rule, int length, int min = 0, int max = 0)
+        static int[] GenerateMassive(Rule rule, int length, int min, int max)
         {
-            if (min == max)
-                max = length;
-            int[] massive = rule(length, min, max);
+            int[] massive;
+
+            if (min > max)
+                massive = rule(length, max, min);
+            else
+                massive = rule(length, min, max);
+
             return massive;
         }
     }
